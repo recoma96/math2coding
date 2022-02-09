1870f986dae73a61b1568daf117abe609b50fe7b
Vector-7 <seokbong60@gmail.com>
2022-02-10 03:33:54 +0900

    바람개비 준비중

---
+++ kmo-2019-high-euler-1st-12/solution.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+MODULA = 1_000_000_000 + 7
+
+def solution(N, K):
+    
+
+    return ans
+
+R = []
+for _ in range(int(input())):
+    n, k = map(int, input()[:-1].split())
+    R.append(solution(n, k))
+
+
+for r in R:
+    print(r)
